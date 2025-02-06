b97c68f64beac7b778b78bcb331e95143f995c91
yamin <yamin148@infinity.gmail.com>
2025-02-06 11:38:33 +0700

    added use dimension hook

---
+++ hooks/useDimension.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from 'react';
+
+const useDimension = () => {
+  const [dimensions, setDimensions] = useState({
+    width: typeof window !== 'undefined' ? window.innerWidth : 0,
+    height: typeof window !== 'undefined' ? window.innerHeight : 0,
+  });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setDimensions({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+
+    // Add event listener for window resize
+    window.addEventListener('resize', handleResize);
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return dimensions;
+};
+
+export default useDimension;
