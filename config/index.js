f573de109581720ca4c2b037da8f472592261628
yamin <yamin148@infinity.gmail.com>
2025-02-07 16:02:07 +0700

    added skills

--- config/index.js
+++ config/index.js
@@ -1,27 +1,95 @@
 export const menu = [
     {
         id: 1,
+        title: 'About',
+        url: '/#about'
+    },
+    {
+        id: 2,
         title: 'Services',
         url: '/#services'
     },
     {
-        id: 2,
-        title: 'Technologies',
-        url: '/#technologies'
+        id: 3,
+        title: 'Skills',
+        url: '/#skills'
     },
     {
-        id: 3,
+        id: 4,
         title: 'Resume',
         url: '/#resume'
     },
     {
-        id: 4,
+        id: 5,
         title: 'Portfolio',
         url: '/#portfolio'
     },
     {
-        id: 5,
+        id: 6,
         title: 'Contact',
         url: '/#contact'
     },
-]
+];
+
+export const skills = [
+    {
+        id: 1,
+        title: 'HTML',
+        icon: '/skills/html-5.png'
+    },
+    {
+        id: 2,
+        title: 'CSS',
+        icon: '/skills/css-3.png'
+    },
+    {
+        id: 3,
+        title: 'Bootstrap',
+        icon: '/skills/bootstrap.png'
+    },
+    {
+        id: 4,
+        title: 'Tailwind CSS',
+        icon: '/skills/tailwind.webp'
+    },
+    {
+        id: 5,
+        title: 'jQuery',
+        icon: '/skills/jquery.svg'
+    },
+    {
+        id: 6,
+        title: 'JavaScript',
+        icon: '/skills/js.png'
+    },
+    {
+        id: 7,
+        title: 'PHP',
+        icon: '/skills/php.png'
+    },
+    {
+        id: 8,
+        title: 'Laravel',
+        icon: '/skills/laravel.png'
+    },
+    {
+        id: 9,
+        title: 'React',
+        icon: '/skills/react-js.png'
+    },
+    {
+        id: 10,
+        title: 'NodeJS',
+        icon: '/skills/node-js.png'
+    },
+    {
+        id: 11,
+        title: 'NextJS',
+        icon: '/skills/next.png'
+    },
+    {
+        id: 13,
+        title: 'npm',
+        icon: '/skills/npm.png'
+    }
+];
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
+import Skills from "@/components/Skills";
 
 export default function Home() {
   return (
     <div>
       <Header />
-      <Hero />
+      <div className="w-full flex flex-col justify-center items-center absolute mt-[1vh] sm:mt-0">
+        <Hero />
+        <Skills />
+      </div>
     </div>
   );
 }
