735b8b6ac686b347932bfccace2d0ca91bda6999
yamin <yamin148@infinity.gmail.com>
2025-02-17 14:32:56 +0700

    wrap with the custom cursor

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import AboutMe from "@/components/AboutMe";
 import Contact from "@/components/Contact";
+import CustomCursor from "@/components/CustomCursor";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import Skills from "@/components/Skills";
@@ -7,15 +8,15 @@ import WorkExperience from "@/components/WorkExperience";
 
 export default function Home() {
   return (
-    <div>
+    <CustomCursor>
       <Header />
-      <div className="w-full flex flex-col justify-center items-center absolute mt-[1vh] sm:mt-0">
-        <Hero />
-        <AboutMe />
-        <Skills />
-        <WorkExperience />
-        <Contact />
-      </div>
-    </div>
+        <div className="w-full flex flex-col justify-center items-center absolute mt-[1vh] sm:mt-0">
+          <Hero />
+          <AboutMe />
+          <Skills />
+          <WorkExperience />
+          <Contact />
+        </div>
+    </CustomCursor>
   );
 }
