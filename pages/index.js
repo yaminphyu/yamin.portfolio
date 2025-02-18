ebf8f662ddfc01d75d42727916f199551db066db
yamin <yamin148@infinity.gmail.com>
2025-02-18 14:07:54 +0700

    added scroll up button

--- pages/index.js
+++ pages/index.js
@@ -5,18 +5,26 @@ import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import Skills from "@/components/Skills";
 import WorkExperience from "@/components/WorkExperience";
+import { FaArrowUpLong } from "react-icons/fa6";
+import styles from '@/styles/Index.module.css';
 
 export default function Home() {
   return (
     <CustomCursor>
       <Header />
-        <div className="w-full flex flex-col justify-center items-center absolute mt-[1vh] sm:mt-0">
-          <Hero />
-          <AboutMe />
-          <Skills />
-          <WorkExperience />
-          <Contact />
-        </div>
+      <div className={styles['body-wrapper']}>
+        <Hero />
+        <AboutMe />
+        <Skills />
+        <WorkExperience />
+        <Contact />
+      </div>
+      <div
+        className={styles['scroll-up']}
+        onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+      >
+        <FaArrowUpLong className={styles['scroll-up-icon']} />
+      </div>
     </CustomCursor>
   );
 }
